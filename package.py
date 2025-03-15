6cc48ddf01c4d92d97defe0277adaa9220b1f2ca
ffekirnew <ffekirnew0808@gmail.com>
2025-03-15 11:47:54 +0300

    Minor changes

--- package.py
+++ package.py
@@ -8,14 +8,16 @@ class Package:
     def __init__(self) -> None:
         self._api = API()
 
-    def start_api(self) -> None:
+    def start(self) -> None:
+        LOG.info("Starting Package...")
         self._api.start()
 
     def stop(self) -> None:
+        LOG.info("Stopping Package...")
         self._api.stop()
 
 
 if __name__ == "__main__":
     main = Package()
 
-    main.start_api()
+    main.start()
