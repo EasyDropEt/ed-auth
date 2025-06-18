adb88316877cb7fa64c7189f7da079e18dee0e95
ffekirnew <ffekirnew0808@gmail.com>
2025-06-19 00:18:06 +0300

    feat: Cors

--- src/package.py
+++ src/package.py
@@ -1,4 +1,6 @@
 from ed_domain.common.logging import get_logger
+from fastapi.middleware.cors import CORSMiddleware
+
 from ed_auth.webapi.api import API
 
 LOG = get_logger()
@@ -7,6 +9,13 @@ LOG = get_logger()
 class Package:
     def __init__(self) -> None:
         self._api = API()
+        self._api.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     def start(self) -> None:
         LOG.info("Starting Package...")
